340b2e5eda7a86d1068bbe1c6eb5edbeddcddc74
gitSushi <lossushi37@gmail.com>
2020-09-03 18:17:02 +0200

    Updates

---
+++ precache-manifest.c73c168fa906ce416788db1471d48e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382a6d72b13a695044b2df1a812475ad",
+    "url": "/the-daily-journo/index.html"
+  },
+  {
+    "revision": "7c23e13f5d258f224b96",
+    "url": "/the-daily-journo/static/css/main.356da6d3.chunk.css"
+  },
+  {
+    "revision": "a582d76e7bffbbe25804",
+    "url": "/the-daily-journo/static/js/2.b3c437bc.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/the-daily-journo/static/js/2.b3c437bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c23e13f5d258f224b96",
+    "url": "/the-daily-journo/static/js/main.8df1be59.chunk.js"
+  },
+  {
+    "revision": "5fd2ff700dc7c72d8752",
+    "url": "/the-daily-journo/static/js/runtime-main.72dabf57.js"
+  }
+]);
